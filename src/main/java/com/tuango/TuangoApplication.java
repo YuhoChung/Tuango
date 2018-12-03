20ea2224bd48feac4b00f53a70f9ea8b62ed0b44
Yuho Chung <764402149@163.com>
2018-12-04 00:51:33 +0800

    1

---
+++ src/main/java/com/tuango/TuangoApplication.java
@@ -0,0 +1,13 @@
+package com.tuango;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuangoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TuangoApplication.class, args);
+
+    }
+}
